e37f7419d5802e264bb6dc2cd0b2ba651d20469e
Mahfuzar Rahman <113121842+Mahfuzar148@users.noreply.github.com>
2023-08-21 20:32:05 +0600

    Update Min heap.cpp

--- heap/Min heap.cpp
+++ heap/Min heap.cpp
@@ -1,91 +1,48 @@
-// C++ program for building Heap from Array
-
 #include <bits/stdc++.h>
-
 using namespace std;
 
-// To heapify a subtree rooted with node i which is
-// an index in arr[]. N is size of heap
-void heapify(int arr[], int N, int i)
-{
-	int largest = i; // Initialize largest as root
-	int l = 2 * i + 1; // left = 2*i + 1
-	int r = 2 * i + 2; // right = 2*i + 2
+void heapify(int arr[], int N, int i) {
+    int smallest = i;
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
 
-	// If left child is larger than root
-	if (l < N && arr[l] > arr[largest])
-		largest = l;
+    if (l < N && arr[l] < arr[smallest])
+        smallest = l;
 
-	// If right child is larger than largest so far
-	if (r < N && arr[r] > arr[largest])
-		largest = r;
+    if (r < N && arr[r] < arr[smallest])
+        smallest = r;
 
-	// If largest is not root
-	if (largest != i) {
-		swap(arr[i], arr[largest]);
-
-		// Recursively heapify the affected sub-tree
-		heapify(arr, N, largest);
-	}
+    if (smallest != i) {
+        swap(arr[i], arr[smallest]);
+        heapify(arr, N, smallest);
+    }
 }
 
-// Function to build a Max-Heap from the given array
-void buildHeap(int arr[], int N)
-{
-	// Index of last non-leaf node
-	int startIdx = (N / 2) - 1;
-
-	// Perform reverse level order traversal
-	// from last non-leaf node and heapify
-	// each node
-	for (int i = startIdx; i >= 0; i--) {
-		heapify(arr, N, i);
-	}
+void buildHeap(int arr[], int N) {
+    int startIdx = (N / 2) - 1;
+    for (int i = startIdx; i >= 0; i--) {
+        heapify(arr, N, i);
+    }
 }
 
-// A utility function to print the array
-// representation of Heap
-void printHeap(int arr[], int N)
-{
-	cout << "Array representation of Heap is:\n";
-
-	for (int i = 0; i < N; ++i)
-		cout << arr[i] << " ";
-	cout << "\n";
+void printHeap(int arr[], int N) {
+    cout << "Array representation of Heap is:\n";
+    for (int i = 0; i < N; ++i)
+        cout << arr[i] << " ";
+    cout << "\n";
 }
 
-// Driver Code
-int main()
-{
-	// Binary Tree Representation
-	// of input array
-	//			 1
-	//		 / \
-	//		 3	 5
-	//	 / \	 / \
-	//	 4	 6 13 10
-	// / \ / \
-	// 9 8 15 17
-	int arr[100] = {100,1, 3, 5,500, 4, 6, 13, 10,150, 9, 8, 15, 17};
+int main() {
+    int arr[100] = {100, 1, 3, 5, 500, 4, 6, 13, 10, 150, 9, 8, 15, 17};
 
-	int n = sizeof(arr) / sizeof(arr[0]);
-	int N = n-count(arr,arr+n,0);
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int N = n - count(arr, arr + n, 0);
 
-	cout<<N<<endl;
+    cout << N << endl;
 
-	// Function call
-	buildHeap(arr, N);
-	printHeap(arr, N);
+    buildHeap(arr, N);
+    printHeap(arr, N);
 
-	// Final Heap:
-	//			 17
-	//		 / \
-	//		 15	 13
-	//		 / \	 / \
-	//	 9	 6 5 10
-	//	 / \ / \
-	//	 4 8 3 1
-
-	return 0;
+    return 0;
 }
 
